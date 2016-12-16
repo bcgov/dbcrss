0d082080e6455e760929fc014096209d82bcae21
BK01 <BK01@users.noreply.github.com>
2016-12-16 08:42:19 -0800

    Update Heartbeat.py

--- heartbeat/src/Heartbeat.py
+++ heartbeat/src/Heartbeat.py
@@ -24,6 +24,7 @@
  Two of the APIs below require the use of API keys. To acquire an API
  key for your application please contact DataBC.
 
+
  Contact Us
  -----------
  https://forms.gov.bc.ca/databc-contact-us/
