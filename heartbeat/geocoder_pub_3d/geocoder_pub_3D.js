ccc8f8ea6d4b335ef5ee11134c536d9f779596ca
BK01 <BK01@users.noreply.github.com>
2016-12-20 12:04:19 -0800

    Update geocoder_pub_3D.js

--- heartbeat/geocoder_pub_3d/geocoder_pub_3D.js
+++ heartbeat/geocoder_pub_3d/geocoder_pub_3D.js
@@ -1,7 +1,8 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/bcgov/dbcrss/master/heartbeat/src/test2.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-line1.csv', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) 
           { return row[key]; }); }
+          
 var x = unpack(rows , 'x');
 var y = unpack(rows , 'y');
 var z = unpack(rows , 'z'); 
